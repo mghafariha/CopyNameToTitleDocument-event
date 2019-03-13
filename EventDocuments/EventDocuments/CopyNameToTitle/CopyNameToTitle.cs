50656c5a9c40880aa524647cf4f76cf3d3a3cbc0
m.ghafariha <m.ghafariha@gmail.com>
2019-03-13 15:10:08 +0330

    new version

---
+++ EventDocuments/EventDocuments/CopyNameToTitle/CopyNameToTitle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace EventDocuments.CopyNameToTitle
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class CopyNameToTitle : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item is being added.
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdding(properties);
+
+            SPWeb web = properties.Web;
+            SPListItem item = properties.ListItem;
+            item["Title"] =item["FileLeafRef"];
+            item.Update();
+        }
+
+        /// <summary>
+        /// An item is being updated.
+        /// </summary>
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            base.ItemUpdating(properties);
+            SPWeb web = properties.Web;
+            SPListItem item = properties.ListItem;
+            item["Title"] = item["FileLeafRef"];
+            item.Update();
+        }
+
+
+    }
+}
---
+++ EventDocuments/EventDocuments/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="e3b858c6-f6d4-467e-a136-b7570e980aac" featureId="e3b858c6-f6d4-467e-a136-b7570e980aac" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="EventDocuments Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="072b79d2-e5b9-476b-bd52-9dbda6f6e802" />
+  </projectItems>
+</feature>
